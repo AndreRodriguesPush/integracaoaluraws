4fa70b7852ffff1d0aaefa5d19c888ee086304f6
AndreThiagoPR <andrethyagopr@gmail.com>
2018-03-21 17:09:38 -0300

    meu primeiro commit

---
+++ estoquews/src/br/com/caelum/estoque/ws/PublicaWebService.java
@@ -0,0 +1,21 @@
+package br.com.caelum.estoque.ws;
+
+import javax.xml.ws.Endpoint;
+
+public class PublicaWebService {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		EstoqueWS service = new EstoqueWS();
+		String url = "http://localhost:8080/estoquews";
+		
+		System.out.print("Serviço Rodando "+url+"wsdl?");
+		
+		Endpoint.publish(url, service);
+		
+		
+
+	}
+
+}
